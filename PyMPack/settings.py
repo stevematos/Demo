7e96ae121f0a05f81ed57309ca24d19349327121
stevematos <s.s.m.m_98@hotmail.com>
2018-09-02 21:25:58 -0500

    Add files para deploy en heroku v1.4

--- PyMPack/settings.py
+++ PyMPack/settings.py
@@ -127,4 +127,4 @@ STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
